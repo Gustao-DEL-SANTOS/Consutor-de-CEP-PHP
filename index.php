3c544cf10de6f0e292af9ccf4b92cdbb4c551571
Gustavo <gustavodelsantos15@gmail.com>
2022-06-22 16:18:48 -0300

    Consultor de CEP PHP finalizado

--- index.php
+++ index.php
@@ -1,39 +1,68 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    <?php 
+        $cep = $_POST['cep'];        
+        $url = "https://viacep.com.br/ws/".$cep."//json/";
+    ?>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Consultor de CEP</title>
+    <link rel="stylesheet" href="./css/style.css" type="text/css">
 </head>
 <body>
     <header>
-        <h1>Consultor de CEP</h1>
+        <h1>Dados do CEP: <?php echo $cep; ?></h1>
     </header>
     
     <?php
 
-        $cep = $_GET['c'];
-        // Fazendo a conexão
-        $url = "https://viacep.com.br/ws/".$cep."//json/";
-        $ch =  curl_init($url);
+    function buscarCep($u){
+                // Fazendo a conexão - iniciando
+                $ch =  curl_init();
         
-        // Retorna no formato de lista
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+                // Indicando qual a URL para se conectar
+                curl_setopt($ch, CURLOPT_URL, $u);
         
-        // A utilização do CURLOPT_SSL_VERIFYPEER é para tentar desviar de alguns problemas que 
-        // que possam ocorrer ao consultar APIs de sites com HTTPS. Para nao fazer verificação.
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-        $result = json_decode(curl_exec($ch), true);
-        var_dump($result);
-        foreach($result as $dados){
-            echo "<h3>$dados<h3>";
-        }
-
-
+                // Retorna no formato de string-lista
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $dadosRetorno = curl_exec($ch);
+                
+                // Visualisar dados no formato Json
+                //var_dump($dadosR);
         
+                // Trabalhando com o retorno dos dados ---- Transformando em uma matriz
+                $dados = json_decode($dadosRetorno, true);
+        
+                echo "<div class=retorno>".
+                        "CEP: "         .$dados["cep"].         "<br/>".
+                        "LOGRADOURO: "  .$dados["logradouro"].  "<br/>".
+                        "COMPLEMENTO: " .$dados["complemento"]. "<br/>".
+                        "BAIRRO: "      .$dados["bairro"].      "<br/>".
+                        "LOCALIDADE: "  .$dados["localidade"].  "<br/>".
+                        "UF: "          .$dados["uf"].          "<br/>".
+                        "IBGE: "        .$dados["ibge"].        "<br/>".
+                        "GIA: "         .$dados["gia"].         "<br/>".
+                        "DDD: "         .$dados["ddd"].         "<br/>".
+                        "SIAFI: "       .$dados["siafi"].       "<br/>".
+                     "</div>";
+        
+                // Fechando a conexão para evitar problemas
+                curl_close($ch); 
+    }
+
+    if(isset($cep) && $cep ==""){
+        echo "<div class=retorno> <h3> Nenhum valor foi inserido! </h3> </div>";
+    }else{
+
+        buscarCep($url);
+
+    }
+       
 
     ?>
+    <a href="index.html">Nova consulta</a>
 </body>
 </html>
